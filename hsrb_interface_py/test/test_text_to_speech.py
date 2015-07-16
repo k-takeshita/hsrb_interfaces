06af5b86b52568ef487f486aa55bb07af164fe87
Akiyoshi Ochiai <akiyoshi_ochiai@mail.toyota.co.jp>
2015-07-16 20:04:21 +0900

    Add test stubs

---
+++ hsrb_interface_py/test/test_text_to_speech.py
@@ -0,0 +1,25 @@
+from nose.tools import ok_, eq_, raises
+from mock import patch, call
+
+import hsrb_interface
+import hsrb_interface.text_to_speech
+
+
+@patch.object(hsrb_interface.Robot, '_connection')
+@patch('hsrb_interface.settings.get_entry')
+@patch('hsrb_interface.utils.CachingSubscriber')
+def test_battery(mock_sub_class, mock_get_entry, mock_connection):
+    mock_connection.return_value = True
+    mock_sub_instance = mock_sub_class.return_value
+    mock_get_entry.return_value = {
+        'topic': 'hoge'
+    }
+    battery = hsrb_interface.battery.Battery('foo')
+    mock_get_entry.assert_called_with('power_supply', 'foo')
+    mock_sub_class.assert_called_with('hoge', BatteryState)
+
+    mock_sub_instance.data.power = 100.0
+    mock_sub_instance.data.temperature = 25.0
+
+    eq_(battery.charge, 100.0)
+    eq_(battery.temperature, 25.0)
