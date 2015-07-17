9a40ff8c63e67435bc1888fc92bea560b2027ed9
西野 環 <tamaki_nishino@mail.toyota.co.jp>
2015-07-17 10:31:25 +0900

    support install_requires

--- hsrb_interface_py/setup.py
+++ hsrb_interface_py/setup.py
@@ -1,9 +1,15 @@
-from distutils.core import setup
+import sys
+from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['hsrb_interface'],
     package_dir={'': 'src'}
 )
+
+install_requires = ['PyYAML']
+if sys.version_info[0] < 3:
+    install_requires.append('enum34')
+d['install_requires'] = install_requires
 
 setup(**d)
