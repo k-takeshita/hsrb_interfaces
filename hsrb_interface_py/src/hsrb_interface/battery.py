a4d05f6489d6cee3bf2d6be02266f17781c8161a
Akiyoshi Ochiai <akiyoshi_ochiai@mail.toyota.co.jp>
2015-07-17 20:20:35 +0900

    Remove unnecessary print statement

--- hsrb_interface_py/src/hsrb_interface/battery.py
+++ hsrb_interface_py/src/hsrb_interface/battery.py
@@ -18,7 +18,6 @@ class Battery(robot.Resource):
     def __init__(self, name):
         super(Battery, self).__init__()
         self._setting = settings.get_entry('power_supply', name)
-        print self._setting
         topic = self._setting['topic']
         self._sub = utils.CachingSubscriber(topic, BatteryState)
 
