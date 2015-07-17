8b43ce48aa3f2db4f68deec373e9a582df81717a
Akiyoshi Ochiai <akiyoshi_ochiai@mail.toyota.co.jp>
2015-07-17 22:09:05 +0900

    Fix build errors

--- hsrb_interface_py/setup.py
+++ hsrb_interface_py/setup.py
@@ -1,15 +1,9 @@
-import sys
-from setuptools import setup
+from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['hsrb_interface'],
     package_dir={'': 'src'}
 )
-
-install_requires = ['PyYAML']
-if sys.version_info[0] < 3:
-    install_requires.append('enum34')
-d['install_requires'] = install_requires
 
 setup(**d)
