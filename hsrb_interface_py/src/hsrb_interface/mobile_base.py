677c4a31d5006621ecacdd7b3878dba67b223174
Akiyoshi Ochiai <akiyoshi_ochiai@mail.toyota.co.jp>
2015-07-21 22:08:06 +0900

    Split MobileBase interface

--- hsrb_interface_py/src/hsrb_interface/mobile_base.py
+++ hsrb_interface_py/src/hsrb_interface/mobile_base.py
@@ -30,25 +30,10 @@ class MobileBase(robot.Resource):
         self._action_client = actionlib.SimpleActionClient(self._setting['move_base_action'], MoveBaseAction)
 
 
-    def goto(self, *args, **kwargs):
-        u"""指定した姿勢まで移動する
-
+    def move(self, pose, timeout=0.0, ref_frame_id=None):
+        u"""
         Args:
-            *args: 引数の数で解釈が変わる。
-
-                ``len(args) == 1``
-                   - ``pose`` (*Tuple[Vector3[m], Quaternion]*): ``ref_frame_id`` から見た目標姿勢
-
-                ``len(args) == 2``
-                   - ``position`` (*Vector3*): ``ref_frame_id`` から見た目標位置
-                   - ``orientaion`` (*Quaternion*): ``ref_frame_id`` から見た目標方位
-
-                ``len(args) == 3``
-                   - ``x``   (*float*): X軸座標[m]
-                   - ``y``   (*float*): Y軸座標[m]
-                   - ``yaw`` (*float*): ヨー軸座標[rad]
-
-        Keyword Args:
+            pose (Tuple[Vector3[m], Quaternion]): ``ref_frame_id`` から見た目標姿勢
             timeout (float): 移動のタイムアウト[sec]（省略時は0となり、無期限に待つ）
             ref_frame_id (str): ゴールの基準座標系名(省略時は ``map`` 座標系）
 
@@ -60,23 +45,11 @@ class MobileBase(robot.Resource):
 
                 with Robot() as robot:
                     base = robot.get('omni_base', robot.Items.MOBILE_BASE)
-                    base.goto(((0.1, 0.2, 0.0), (0.0, 0.0, 0.0, 1.0)), 10.0, 'base_footprint')
+                    pose = (Vector3(0.1, 0.2, 0.0), Quaternion(0.0, 0.0, 0.0, 1.0)
+                    base.move(pose, 10.0, 'base_footprint')
         """
-        if len(args) == 1:
-            pose = args[0]
-        elif len(args) == 2:
-            pose = (args[0], args[1])
-        elif len(args) == 3:
-            x, y, yaw = args
-            position = geometry.Vector3(x, y, 0)
-            q = tf.transformations.quaternion_from_euler(0, 0, yaw)
-            orientation = geometry.Quaternion(*q)
-            pose = (position, orientation)
-        else:
-            raise ValueError("")
-
-        timeout = kwargs.get('timeout', 0.0)
-        ref_frame_id = kwargs.get('ref_frame_id', settings.get_frame('map'))
+        if ref_frame_id is None:
+            ref_frame_id = settings.get_frame('map')
 
         target_pose = PoseStamped()
         target_pose.header.frame_id = ref_frame_id
@@ -89,6 +62,56 @@ class MobileBase(robot.Resource):
         self._action_client.wait_for_result(rospy.Duration(timeout))
         if self._action_client.get_state() != actionlib.GoalStatus.SUCCEEDED:
             raise exceptions.MobileBaseError('Failed to reach goal')
+
+    @property
+    def pose(self):
+        u"""``map`` 座標系での自己位置推定値
+
+        Returns:
+            List[float]: (x[m], y[m], yaw[rad])
+        """
+        pose = self._pose_sub.data
+        x = pose.pose.position.x
+        y = pose.pose.position.y
+        q = [ pose.pose.orientation.x,
+              pose.pose.orientation.y,
+              pose.pose.orientation.z,
+              pose.pose.orientation.w ]
+        euler_angles = tf.transformations.euler_from_quaternion(q)
+        yaw = euler_angles[2]
+        return [x, y, yaw]
+
+    def go(self, x, y, yaw, timeout=0.0, relative=False):
+        u"""指定した座標まで移動する
+
+        Args:
+            x   (float): X軸座標[m]
+            y   (float): Y軸座標[m]
+            yaw (float): ヨー軸座標[rad]
+            timeout (float): 移動のタイムアウト[sec]（省略時は0となり、無期限に待つ）
+            relative (bool): ``True`` ならロボット基準座標系で移動する。``False`` なら ``map`` 座標系。
+
+        Returns:
+            None
+
+        Example:
+            Usage::
+
+                with Robot() as robot:
+                    base = robot.get('omni_base', robot.Items.MOBILE_BASE)
+                    base.go(0.1, 0.2, 0.0, 10.0)
+        """
+        position = geometry.Vector3(x, y, 0)
+        q = tf.transformations.quaternion_from_euler(0, 0, yaw)
+        orientation = geometry.Quaternion(*q)
+        pose = (position, orientation)
+
+        if relative:
+            ref_frame_id = settings.get_frame('base')
+        else:
+            ref_frame_id = settings.get_frame('map')
+        self.move(pose, timeout, ref_frame_id)
+
 
     @property
     def pose(self):
