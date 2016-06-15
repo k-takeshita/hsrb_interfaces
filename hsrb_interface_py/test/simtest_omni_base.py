05619033b216376dbe294bc75f12e6955a5e1612
Akiyoshi Ochiai <akiyoshi_ochiai@mail.toyota.co.jp>
2016-06-15 16:16:17 +0900

    Simluation test about omni_base passed

--- hsrb_interface_py/test/simtest_omni_base.py
+++ hsrb_interface_py/test/simtest_omni_base.py
@@ -3,26 +3,28 @@
 """
 
 """
+import math
 import unittest
 from tf import transformations
 from hsrb_interface import geometry
 import testing
 
 class OmniBaseTest(testing.HsrbInterfaceTest):
-
     def test_simple_move(self):
         """Getting started"""
+        self.whole_body.move_to_go()
+        self.omni_base.go(0, 0, 0, self.BASE_MOVE_TIME_TOLERANCE, relative=False)
         self.omni_base.go(0.1, 0.0, 0.0, self.BASE_MOVE_TIME_TOLERANCE,
                           relative=True)
-        self.expect_pose_equal(self.omni_base.get_pose('map'),
-                               geometry.pose(0.1, 0.0, 0.0),
-                               delta=self.BASE_MOVE_GOAL_TOLERANCE)
+        self.expect_base_reach_goal(geometry.pose(0.1, 0.0, 0.0), 'map',
+                                    pos_delta=0.05, ori_delta=0.05,
+                                    timeout=self.BASE_MOVE_TIME_TOLERANCE)
 
         self.omni_base.go(-0.1, 0.0, 0.0, self.BASE_MOVE_TIME_TOLERANCE,
                           relative=True)
-        self.expect_pose_equal(self.omni_base.get_pose('map'),
-                               geometry.pose(0.0, 0.0, 0.0),
-                               delta=self.BASE_MOVE_GOAL_TOLERANCE)
+        self.expect_base_reach_goal(geometry.pose(0.0, 0.0, 0.0), 'map',
+                                    pos_delta=0.05, ori_delta=0.05,
+                                    timeout=self.BASE_MOVE_TIME_TOLERANCE)
 
     def test_get_current_position(self):
         """Get current position"""
@@ -41,44 +43,53 @@ class OmniBaseTest(testing.HsrbInterfaceTest):
     def test_move_globally(self):
         """Move to global position"""
         self.whole_body.move_to_go()
-        self.omni_base.go(6.0, 6.0, 0.0, 300.0, relative=False)
-        self.expect_pose_equal(self.omni_base.get_pose('map'),
-                               geometry.pose(6.0, 6.0, 0.0),
-                               delta=self.BASE_MOVE_GOAL_TOLERANCE)
+        self.omni_base.go(6.0, 4.0, 0.0, 300.0, relative=False)
+        self.expect_base_reach_goal(geometry.pose(6.0, 4.0, 0.0), 'map',
+                                    pos_delta=0.05, ori_delta=0.05,
+                                    timeout=self.BASE_MOVE_TIME_TOLERANCE)
 
         self.omni_base.go(0.0, 0.0, 0.0, 300.0, relative=False)
-        self.expect_pose_equal(self.omni_base.get_pose('map'),
-                               geometry.pose(0.0, 0.0, 0.0),
-                               delta=self.BASE_MOVE_GOAL_TOLERANCE)
-    #@unittest.skip('test')
+        self.expect_base_reach_goal(geometry.pose(0.0, 0.0, 0.0), 'map',
+                                    pos_delta=0.05, ori_delta=0.05,
+                                    timeout=self.BASE_MOVE_TIME_TOLERANCE)
+
     def test_move_relatively(self):
         """Move to relative position"""
         self.whole_body.move_to_go()
 
         self.omni_base.go(0.0, 0.0, 0.0, 300.0, relative=False)
-        self.expect_pose_equal(self.omni_base.get_pose('map'),
-                               geometry.pose(0.0, 0.0, 0.0),
-                               delta=self.BASE_MOVE_GOAL_TOLERANCE)
+        self.expect_base_reach_goal(geometry.pose(0.0, 0.0, 0.0), 'map',
+                                    pos_delta=0.05, ori_delta=0.05,
+                                    timeout=self.BASE_MOVE_TIME_TOLERANCE)
 
         self.omni_base.go(0.0, 1.0, 0.0, 300.0, relative=True)
-        self.expect_pose_equal(self.omni_base.get_pose('map'),
-                               geometry.pose(0.0, 1.0, 0.0),
-                               delta=self.BASE_MOVE_GOAL_TOLERANCE)
+        self.expect_base_reach_goal(geometry.pose(0.0, 1.0, 0.0), 'map',
+                                    pos_delta=0.05, ori_delta=0.05,
+                                    timeout=self.BASE_MOVE_TIME_TOLERANCE)
 
-    @unittest.skip('test')
+        self.omni_base.go(0.0, -1.0, 0.0, 300.0, relative=True)
+        self.expect_base_reach_goal(geometry.pose(0.0, 0.0, 0.0), 'map',
+                                    pos_delta=0.05, ori_delta=0.05,
+                                    timeout=self.BASE_MOVE_TIME_TOLERANCE)
+
     def test_move_using_tf(self):
-        """Relative move using tf"""
+        """Relative move using tf
+
+        my_frame is published as '2.0 0.0 0.5 3.14 -1.57 0.0 map my_frame 10'
+        """
         self.whole_body.move_to_go()
 
-        goal = geometry.pose(ej=-1.57)
+        goal = geometry.pose(ej=math.pi/2.0)
         self.omni_base.move(goal, 100.0, ref_frame_id='my_frame')
-        self.expect_pose_equal(self.omni_base.get_pose('my_frame'), goal,
-                               delta=self.BASE_MOVE_GOAL_TOLERANCE)
+        self.expect_base_reach_goal(goal, frame='my_frame',
+                                    pos_delta=0.05, ori_delta=0.05,
+                                    timeout=self.BASE_MOVE_TIME_TOLERANCE)
 
-        goal = geometry.pose(z=-0.5, ej=-1.57)
+        goal = geometry.pose(z=-0.5, ej=math.pi/2.0)
         self.omni_base.move(goal, 100.0, ref_frame_id='my_frame')
-        self.expect_pose_equal(self.omni_base.get_pose('my_frame'), goal,
-                               delta=self.BASE_MOVE_GOAL_TOLERANCE)
+        self.expect_base_reach_goal(goal, frame='my_frame',
+                                    pos_delta=0.05, ori_delta=0.05,
+                                    timeout=self.BASE_MOVE_TIME_TOLERANCE)
 
 if __name__ == '__main__':
     import rostest
