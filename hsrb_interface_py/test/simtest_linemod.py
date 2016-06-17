0a1eb0161bc13fa9f9c89f57ac98d033725a3102
Akiyoshi Ochiai <akiyoshi_ochiai@mail.toyota.co.jp>
2016-06-17 15:47:55 +0900

    Add LINEMOD simulation testing

---
+++ hsrb_interface_py/test/simtest_linemod.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# vim: fileencoding=utf-8 :
+"""
+
+"""
+import math
+import rospy
+import unittest
+from tf import transformations
+from hsrb_interface import geometry
+import testing
+
+class LinemodTest(testing.HsrbInterfaceTest):
+    def test_detect_bottle(self):
+        """Getting started"""
+        self.whole_body.move_to_go()
+        self.omni_base.go(0.5, 1.5, 0.0, relative=True)
+        self.whole_body.move_to_go()
+        self.expect_object(44, geometry.pose(1.35, 1.52, 0.7, ek=math.pi/2.0),
+                           pos_delta=0.15, frame='map')
+
+if __name__ == '__main__':
+    import rostest
+    rostest.rosrun('hsrb_interface_py', 'simtest_linemod', LinemodTest)
