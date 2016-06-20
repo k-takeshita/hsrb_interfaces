6fd82585d47f33043329376b71f92b1157ff5773
Akiyoshi Ochiai <akiyoshi_ochiai@mail.toyota.co.jp>
2016-06-20 19:46:46 +0900

    Change a reference measurement frame in simulation marker test

--- hsrb_interface_py/test/simtest_marker.py
+++ hsrb_interface_py/test/simtest_marker.py
@@ -16,9 +16,9 @@ class MarkerTest(testing.HsrbInterfaceTest):
         self.whole_body.move_to_go()
         self.omni_base.go(0.5, 1.5, 0.0, relative=True)
         self.whole_body.move_to_go()
-        self.expect_object(27, geometry.pose(1.35, 1.52, 0.7, ek=math.pi/2.0),
-                           pos_delta=0.03, ori_delta=math.radians(5),
-                           frame='map')
+        self.expect_object(27, geometry.pose(0.85, 0.02, 0.7, ek=math.pi/2.0),
+                           pos_delta=0.05, ori_delta=math.radians(5),
+                           frame='base_footprint')
 
 
 if __name__ == '__main__':
