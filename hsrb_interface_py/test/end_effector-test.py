83f9aee854ad419d10532acf677d60410d45b0b7
水野 貴文 <takafumi_mizuno_zb@mail.toyota.co.jp>
2017-03-22 09:57:10 +0900

    add suction command test

--- hsrb_interface_py/test/end_effector-test.py
+++ hsrb_interface_py/test/end_effector-test.py
@@ -5,6 +5,7 @@ import hsrb_interface
 import hsrb_interface.end_effector
 
 from mock import patch
+from nose.tools import assert_raises
 from nose.tools import ok_
 
 
@@ -33,6 +34,23 @@ def test_suction(mock_sub_class, mock_get_entry, mock_connecting):
         "suction_topic": "/suction_on",
         "pressure_sensor_topic": "/pressure_sensor"
     }
-    battery = hsrb_interface.end_effector.Suction('foo')
-    ok_(battery)
+    suction = hsrb_interface.end_effector.Suction('foo')
+    ok_(suction)
     mock_get_entry.assert_called_with('end_effector', 'foo')
+
+
+@patch.object(hsrb_interface.Robot, '_connecting')
+@patch('hsrb_interface.settings.get_entry')
+@patch('hsrb_interface.utils.CachingSubscriber')
+def test_suction_command_negative_num(mock_sub_class,
+                                      mock_get_entry,
+                                      mock_connecting):
+    """Test Suction command(negative number)"""
+    mock_connecting.return_value = True
+    mock_get_entry.return_value = {
+        "action": "/suction_control",
+        "suction_topic": "/suction_on",
+        "pressure_sensor_topic": "/pressure_sensor"
+    }
+    suction = hsrb_interface.end_effector.Suction('foo')
+    assert_raises(ValueError, suction.command, -1)
