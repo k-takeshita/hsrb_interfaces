4e43367c2d524518e7d8ded75c91644b637a2825
Keisuke Takeshita <keisuke_takeshita@mail.toyota.co.jp>
2017-05-16 08:03:33 +0900

    Fix end_effector-test.py to suit actionlib 1.11.8.

--- hsrb_interface_py/test/end_effector-test.py
+++ hsrb_interface_py/test/end_effector-test.py
@@ -2,76 +2,55 @@
 """Unittest for end_effector module"""
 
 import hsrb_interface
+from hsrb_interface import _testing as testing
 import hsrb_interface.end_effector
 
-from mock import patch
 from nose.tools import assert_raises
 from nose.tools import ok_
 
 from std_msgs.msg import Bool
 
 
-@patch.object(hsrb_interface.Robot, '_connecting')
-@patch('hsrb_interface.settings.get_entry')
-def test_gripper(mock_get_entry, mock_connecting):
-    """Test Gripper class"""
-    mock_connecting.return_value = True
-    mock_get_entry.return_value = {
-        'topic': 'hoge',
-        "joint_names": ["hand_motor_joint"],
-        "prefix": "/hsrb/gripper_controller"
-    }
-    gripper = hsrb_interface.end_effector.Gripper('foo')
-    ok_(gripper)
+class EndEffectorTest(testing.RosMockTestCase):
 
+    def setUp(self):
+        super(EndEffectorTest, self).setUp()
+        self.gripper_setting = {
+            'topic': 'hoge',
+            "joint_names": ["hand_motor_joint"],
+            "prefix": "/hsrb/gripper_controller"
+        }
+        self.suction_setting = {
+            "action": "/suction_control",
+            "suction_topic": "/suction_on",
+            "pressure_sensor_topic": "/pressure_sensor"
+        }
 
-@patch.object(hsrb_interface.Robot, '_connecting')
-@patch('hsrb_interface.settings.get_entry')
-@patch('hsrb_interface.utils.CachingSubscriber')
-def test_suction(mock_sub_class, mock_get_entry, mock_connecting):
-    """Test Suction class"""
-    mock_connecting.return_value = True
-    mock_get_entry.return_value = {
-        "action": "/suction_control",
-        "suction_topic": "/suction_on",
-        "pressure_sensor_topic": "/pressure_sensor"
-    }
-    suction = hsrb_interface.end_effector.Suction('foo')
-    ok_(suction)
-    mock_get_entry.assert_called_with('end_effector', 'foo')
+    def test_gripper(self):
+        """Test Gripper class"""
+        self.get_entry_mock.side_effect = [self.gripper_setting]
+        gripper = hsrb_interface.end_effector.Gripper('foo')
+        ok_(gripper)
 
+    def test_suction(self):
+        """Test Suction class"""
+        self.get_entry_mock.side_effect = [self.suction_setting]
+        suction = hsrb_interface.end_effector.Suction('foo')
+        ok_(suction)
+        self.get_entry_mock.assert_called_with('end_effector', 'foo')
 
-@patch.object(hsrb_interface.Robot, '_connecting')
-@patch('hsrb_interface.settings.get_entry')
-@patch('hsrb_interface.utils.CachingSubscriber')
-@patch('rospy.Publisher')
-def test_suction_command(mock_pub_class, mock_sub_class,
-                         mock_get_entry, mock_connecting):
-    """Test Suction command(exceptions are never thrown)"""
-    mock_connecting.return_value = True
-    mock_get_entry.return_value = {
-        "action": "/suction_control",
-        "suction_topic": "/suction_on",
-        "pressure_sensor_topic": "/pressure_sensor"
-    }
-    suction = hsrb_interface.end_effector.Suction('foo')
-    suction.command(True)
-    suction.command(False)
-    mock_pub_class.assert_called_with("/suction_on", Bool, queue_size=0)
+    def test_suction_command(self):
+        """Test Suction command(exceptions are never thrown)"""
+        self.get_entry_mock.side_effect = [self.suction_setting]
+        suction = hsrb_interface.end_effector.Suction('foo')
+        suction.command(True)
+        suction.command(False)
+        self.publisher_mock.assert_called_with("/suction_on",
+                                               Bool,
+                                               queue_size=0)
 
-
-@patch.object(hsrb_interface.Robot, '_connecting')
-@patch('hsrb_interface.settings.get_entry')
-@patch('hsrb_interface.utils.CachingSubscriber')
-def test_suction_command_negative_num(mock_sub_class,
-                                      mock_get_entry,
-                                      mock_connecting):
-    """Test Suction command(negative number)"""
-    mock_connecting.return_value = True
-    mock_get_entry.return_value = {
-        "action": "/suction_control",
-        "suction_topic": "/suction_on",
-        "pressure_sensor_topic": "/pressure_sensor"
-    }
-    suction = hsrb_interface.end_effector.Suction('foo')
-    assert_raises(ValueError, suction.command, -1)
+    def test_suction_command_negative_num(self):
+        """Test Suction command(negative number)"""
+        self.get_entry_mock.side_effect = [self.suction_setting]
+        suction = hsrb_interface.end_effector.Suction('foo')
+        assert_raises(ValueError, suction.command, -1)
