4da4de7a305784771447fc2f853ac344ec4feea7
Keisuke Takeshita <keisuke_takeshita@mail.toyota.co.jp>
2017-10-09 17:01:45 +0900

    Add inf checking to gaze_point.

--- hsrb_interface_py/src/hsrb_interface/joint_group.py
+++ hsrb_interface_py/src/hsrb_interface/joint_group.py
@@ -878,6 +878,8 @@ class JointGroup(robot.Item):
         Notes:
             If the calculated angle is over the limit, the angle is rounded.
         """
+        if any([math.isinf(x) for x in point]):
+            raise ValueError("The point includes inf.")
         if ref_frame_id is None:
             ref_frame_id = settings.get_frame('base')
         origin_to_ref_ros_pose = self._lookup_odom_to_ref(ref_frame_id)
